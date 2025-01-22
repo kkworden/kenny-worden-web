c4cd16a385671ed234811407abde4462fbc82665
Kenny Worden <kenny.worden@gmail.com>
2025-01-22 00:08:33 -0500

    Fix build

--- src/contexts/vote.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-interface VoteContext {
-  electionCode: string
-  setElectionCode: (electionCode: string) => void
-}
-
-export default createContext<VoteContext>({} as VoteContext);
